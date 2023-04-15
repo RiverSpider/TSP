6fe769702c988891e5e3ca6980bcece61bc3080f
Maxim Raevskikh <84962861+RiverSpider@users.noreply.github.com>
2023-04-15 12:57:35 +0600

    Update BordersMethod.py

--- BordersMethod/BordersMethod.py
+++ BordersMethod/BordersMethod.py
@@ -1,4 +1,3 @@
-import sys
 graph = [
     [0, 14, 55, 7, 5],
     [8, 0, 5, 46, 36],
@@ -6,25 +5,27 @@ graph = [
     [37, 5, 6, 0, 22],
     [6, 25, 31, 38, 0]
 ]
-n = len(graph)
-visited = [False] * n
-path = [0]
-visited[0] = True
-dist = 0
-for i in range(n-1):
-    min_dist = sys.maxsize
-    next = -1
-    current = path[-1]
-        
-    for j in range(n):
-        if not visited[j] and graph[current][j] < min_dist:
-            min_dist = graph[current][j]
-            dist += min_dist
-            next = j  
-    visited[next] = True
-    path.append(next)
-        
-path.append(0)
-print(path, dist)
-#[0, 4, 1, 2, 3, 0] 85
+def getnodes(n):
+    if n == 1:
+        return [[0]]
+    else:
+        nodes = []
+        for node in getnodes(n-1):
+            for i in range(n):
+                nnode = node[:i] + [n-1] + node[i:]
+                nodes.append(nnode)
+        return nodes
 
+n = len(graph)
+allnodes = getnodes(n)
+mdistance = float('inf')
+for node in allnodes:
+    distance = 0
+    for i in range(n-1):
+        distance += graph[node[i]][node[i+1]]
+    distance += graph[node[-1]][node[0]]
+    if distance < mdistance:
+        mdistance = distance
+        route = node
+print(route, mdistance)
+#[4, 2, 3, 5, 1, 4] 29
